a5128cbf81af7275a55a79c3bc10301a9e4bc350
Darío Clavijo <dclavijo@antel.net.uy>
2015-07-22 17:04:20 -0300

    binary search test

---
+++ logsearch.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+#
+# Author Dario Clavijo 2015
+# GPLv3
+
+# linear search
+def seek(A,key,imin,imax):
+	for i in xrange(imin,imax):
+		if A[n] == key:
+			return v
+
+
+count = 0
+
+# logarithm search
+def seeklog(A,key,imin,imax):
+	global count
+	count +=1
+
+	if len(A) > 0:
+		imid = (imax + imin) / 2
+		if A[imid] > key:
+			return seeklog(A,key,imin,imid- 1 )
+		elif A[imid] < key:
+			return seeklog(A,key,imid+1,imax)	
+		else:
+			return imid
+
+def test():
+	tmp = [x for x in xrange(1024*1024*20)]
+	print "res=",seeklog(tmp,1337,0,len(tmp))
+	print "count=",count
+
+test()
