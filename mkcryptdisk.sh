a37aff501c952c10b6edd8a1fe309a311f6d6417
dclavijo <daedalus@users.noreply.github.com>
2016-01-07 17:10:43 -0300

    mkcryptdisk.sh

---
+++ mkcryptdisk.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Author Dario Clavijo 2016
+
+set -x
+
+DISK=$1
+KEYFILE=$2
+
+cryptsetup luksFormat -y -v /dev/$DISK
+cryptsetup luksAddKey /dev/$DISK $KEYFILE
+cryptsetup --key-file $KEYFILE luksOpen /dev/$DISK $DISK
+mkfs.btrfs /dev/mapper/$DISK
+mkdir /media/$DISK
+mount /dev/mapper/$DISK /media/$DISK 
+
+BLKID=$(blkid /dev/$DISK -value | head -n 1)
+
+echo "$DISK $BLKID $KEYFILE  luks" > /etc/crypttab
+echo "/dev/mapper/$DISK /media$DISK btrfs defaults 0 0" > /etc/fstab
