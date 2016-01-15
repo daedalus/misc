e25da61aa987854f7fa684c0acbcd14fe04f7146
Cody <cody@quantifiedcode.com>
2016-01-15 21:26:53 +0000

    Migrated `%` string formating

--- compression_test1.py
+++ compression_test1.py
@@ -7,13 +7,13 @@ import fileinput
 
 def test(data):
         u = len(data)/2
-        print "Uncompressed: %d" % (u)
+        print "Uncompressed: {0:d}".format((u))
 
         for i in range(1,10):
                 c = zlib.compress(data.decode('hex'),i)
                 b = bz2.compress(data.decode('hex'),i)
-                print "zlib compression level: %d, bytes: %d, ratio: %2.2f" % (i,len(c), 100-len(c)/(float(u))*100)
-                print "bz2  compression level: %d, bytes: %d, ratio: %2.2f" % (i,len(b), 100-len(b)/(float(u))*100)
+                print "zlib compression level: {0:d}, bytes: {1:d}, ratio: {2:2.2f}".format(i, len(c), 100-len(c)/(float(u))*100)
+                print "bz2  compression level: {0:d}, bytes: {1:d}, ratio: {2:2.2f}".format(i, len(b), 100-len(b)/(float(u))*100)
                                          
 
 for line in fileinput.input():
