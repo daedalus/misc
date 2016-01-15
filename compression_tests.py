aafead8aec109cb4f0c4cca2f7e5f31130bc1b4f
dclavijo <daedalus@users.noreply.github.com>
2016-01-15 15:26:03 -0300

    Update compression_tests.py

--- compression_tests.py
+++ compression_tests.py
@@ -6,13 +6,14 @@ import bz2
 import fileinput
 
 def test(data):
-	print "Uncompressed: %d" % (len(data)/2)
+        u = len(data)/2
+        print "Uncompressed: %d" % (u)
 
-	for i in range(1,10):
-        	c = zlib.compress(data.decode('hex'),i)
-	        b = bz2.compress(data.decode('hex'),i)
-        	print "zlib compression level: %d bytes: %d" % (i,len(c))
-	        print "bz2  compression level: %d bytes: %d" % (i,len(b))
+        for i in range(1,10):
+                c = zlib.compress(data.decode('hex'),i)
+                b = bz2.compress(data.decode('hex'),i)
+                print "zlib compression level: %d, bytes: %d, ratio: %2.2f" % (i,len(c), 100-len(c)/(float(u))*100)
+                print "bz2  compression level: %d, bytes: %d, ratio: %2.2f" % (i,len(b), 100-len(b)/(float(u))*100)
                                          
 
 for line in fileinput.input():
