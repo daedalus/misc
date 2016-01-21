6b92b8640638df9ca46eef5e2b4442a158ad390a
Dario Clavijo <daedalus2027@gmail.com>
2016-01-21 16:31:21 -0300

    chromium cookie dump script

---
+++ cookiedump.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import os
+import sqlite3
+import pwd
+import sys
+
+def getFromCookieDB(filename):
+	connection = sqlite3.connect(filename)
+        cur = connection.cursor()
+        cur.execute('SELECT * FROM cookies')
+	rows = cur.fetchall()
+    	for row in rows:
+        	print row
+
+def dump(uid):
+	try:
+    		filename = os.path.join(pwd.getpwuid(uid).pw_dir, ".config/chromium/Default/Cookies")
+	except:
+		filename = ""
+    	if filename != "" and os.path.isfile(filename):
+		getFromCookieDB(filename)
+
+
+if len(sys.argv)>1:
+	 dump(int(sys.argv[1]))
+else:
+	for u in pwd.getpwall():
+		dump(u.pw_uid)
