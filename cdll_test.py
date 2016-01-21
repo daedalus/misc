7c0ddfe5d85bd9aeba65e56df42e9de7dc6a1084
Dario Clavijo <daedalus2027@gmail.com>
2016-01-21 16:32:25 -0300

    python cdll test examples

---
+++ cdll_test.py
@@ -0,0 +1,7 @@
+import ctypes
+libc = ctypes.CDLL( 'libc.so.6' )   # under Linux/Unix
+t = libc.time(None)                      # equivalent C code: t = time(NULL)
+print t
+
+m = libc.malloc(1024*1024*10)
+print m
