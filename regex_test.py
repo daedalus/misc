f43ecaae349605d372728ebcd009db9ef7d21a1c
Dario Clavijo <daedalus2027@gmail.com>
2016-07-13 00:02:52 -0300

    regex test

---
+++ regex_test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import re
+import fileinput
+
+data = ""
+
+these_regex="<pre>(.+?)</pre>"
+pattern=re.compile(these_regex)
+
+for line in fileinput.input():
+	data += line.replace("\n","____SEPARATOR____")
+
+ext = re.findall(pattern,data)
+
+for t in ext:
+	print t.replace("____SEPARATOR____","\n")
