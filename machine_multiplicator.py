a9d8ac15b1b938358f9b99421e39fddad76c4e44
dclavijo <clavijodario@gmail.com>
2016-07-21 22:32:23 -0300

    Create machine_multiplicator.py

---
+++ machine_multiplicator.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# The way the machine multiplies
+# Author Dario Clavijo 2016
+# GPLv3
+
+bits = 9
+multiplier = 256
+multiplicand = 256
+
+print multiplicand,"*",multiplier
+
+product = 0
+for i in range(0,bits):
+        if (multiplier & 1) == 1:
+                product = product + multiplicand
+        multiplicand = multiplicand << 1
+        multiplier = multiplier >> 1
+        print i,multiplicand,multiplier,product
+
+print "Result: ",product
