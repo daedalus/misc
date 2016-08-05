e24108d6ba2cb92e10ee64571f87c8632ed24a06
dclavijo <clavijodario@gmail.com>
2016-08-05 17:55:58 -0300

    machine division

---
+++ machine_division.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# The way the machine multiplies
+# Author Dario Clavijo 2016
+# GPLv3
+
+#euclids division by susbstraction
+def MDIV(N,D):
+        C = 0
+        R = N
+        while R >= D:
+                R -= D
+                C += 1
+        return (C,R)
+
+
+print MDIV(10000,3)
