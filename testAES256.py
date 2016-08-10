329e7031a6935d8c32ae5219ca173d74d7c48306
dclavijo <clavijodario@gmail.com>
2016-08-10 15:26:59 -0300

    Update testAES256.py

--- testAES256.py
+++ testAES256.py
@@ -7,38 +7,38 @@ BLOCK_SIZE=16
 
 # MODES CFB,CBC XTS
  
-
-def encrypt(message, passphrase,mode):
+def encrypt(message, passphrase,mode,IV):
     # passphrase MUST be 16, 24 or 32 bytes long, how can I do that ?
-    IV = Random.new().read(BLOCK_SIZE)
-    aes = AES.new(passphrase, mode, IV)
-    return base64.b64encode(IV),base64.b64encode(aes.encrypt(message))
+    aes = AES.new(passphrase, mode, base64.b64decode(IV))
+    return base64.b64encode(aes.encrypt(message))
 
 def decrypt(encrypted, passphrase,mode,IV):
     #IV = Random.new().read(BLOCK_SIZE)
     aes = AES.new(passphrase, mode, base64.b64decode(IV))
     return aes.decrypt(base64.b64decode(encrypted))
 
-
 def makekey(password):
 	return hashlib.sha256(hashlib.sha256(password).digest()).digest()
 
-print 'password = a'
+def makeIV():
+	IV = Random.new().read(BLOCK_SIZE)
+	return base64.b4encode(IV)
 
-key = makekey('a')
+def test():
+	print 'password = a'
+	input_str = '0123456789ABCDEF0123456789ABCDEF'
+	print "input message =",input_str
 
-print 'key = double hashed password =',key.encode('hex')
+	key = makekey('a')
+	print 'key = double hashed password =',key.encode('hex')
 
-input_str = '0123456789ABCDEF0123456789ABCDEF'
+	mode = AES.MODE_CBC
+	print "MODE = ", mode
 
-print "input message =",input_str
+	IV,enc = encrypt(input_str,key,mode,IV)
+	print 'IV =',IV,',encrypted message =',enc
 
-mode = AES.MODE_CBC
-
-print "MODE = ", mode
-
-IV,enc = encrypt(input_str,key,mode)
-print 'IV =',IV,',encrypted message =',enc
-
-dec = decrypt(enc,key,mode,IV)
-print "decrypted message = ",dec
+	dec = decrypt(enc,key,mode,IV)
+	print "decrypted message = ",dec
+	
+test()
