b07c2d7ec1ecae7ae288c54e89db444636fe5a27
Darío Clavijo <clavijodario@gmail.com>
2016-08-14 16:50:26 -0300

    Create miner_config.sh

---
+++ miner_config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+filename="$1"
+POOL="$2"
+USER="$3"
+
+while read -r line
+do
+    reply=$(torify  $HOME/code/BTC/cgminer/cgminer-api "addpool|stratum+tcp://$POOL,$USER.1,xxxx" $line 4028)
+    echo $line $reply >> scan_log	
+done < "$filename"
