b3a0bd107c96980eb3bd77eea4d32c770d8388ac
Darío Clavijo <clavijodario@gmail.com>
2016-08-15 02:03:23 -0300

    Update licencia.py

--- licencia.py
+++ licencia.py
@@ -1,7 +1,4 @@
-# !/usr/bin/env python
-# author Dario Clavijo 2016
- 
-workdays import *
+from workdays import *
 from datetime import date
 
 # calculamos mi licencia sin contar sabados ni domingos ni el 12-10-2016
@@ -11,10 +8,20 @@ def lic():
         days = 28
         franqueo = 3
 
-        r1 = workday(dstart,days,[d12])
-        r2 = workday(dstart,days + franqueo,[d12])
+        r1 = workday(dstart,days)
+        r2 = workday(dstart,days + franqueo)
+        r3 = workday(dstart,days,[d12])
+        r4 = workday(dstart,days + franqueo,[d12])
 
+        print "Sin contar:",d12
         print "Empieza:",dstart,", Dias:",days,", Termina:",r1
-        print "Empieza:",dstart,", Dias:",days+franqueo, ", Termina:",r2
+        print "Empieza:",dstart,", Dias:",days+franqueo, ", Termina:",r1
+        print
+
+        print "Contando:", d12
+        print "Empieza:",dstart,", Dias:",days,", Termina:",r3
+        print "Empieza:",dstart,", Dias:",days+franqueo, ", Termina:",r4
+
 
 lic()
+
