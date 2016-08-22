c098b707cf43e894c7df721a74c894cf13310b37
Darío Clavijo <daedalus2027@gmail.com>
2016-08-21 22:44:33 -0300

    rot13

---
+++ rot13.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# Author Dario Clavijo 2016
+# simple ROT13 algo or Caesar Cipher
+
+def rot13(test):
+	p = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ "
+	r = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM "
+	d = ""
+	for l in test:
+		d +=  r[p.find(l)]
+	return d
+
+print rot13("Dario Clavijo")
