9deb20fae3754bd7a6f2bad56e04aeca1c77a191
Darío Clavijo <clavijodario@gmail.com>
2016-11-21 12:46:46 -0300

    quicksort algo

--- sort.py
+++ sort.py
@@ -11,6 +11,24 @@ def bubblesort(numberset):
 		 		numberset[i],numberset[i+1] = numberset[i+1],numberset[i]		
 	return numberset
 
+def swap(a,b): return b,a 
+
+def partition(A, lo, hi):
+	pivot = A[hi]
+	i = lo     #place for swapping
+	for j in (lo, hi – 1):
+		if A[j] <= pivot:
+       			A[i],A[j] = swap (A[i],A[j])
+			i := i + 1
+    	A[i], A[hi] = swap (A[i], A[hi])
+	return i
+
+def quicksort(A, lo, hi):
+	if lo < hi:
+		p = partition(A, lo, hi)
+        	quicksort(A, lo, p – 1)
+        	quicksort(A, p + 1, hi)
 
 print numbers
 print bubblesort(numbers)
+print quicksort(numbers,0,len(numbers))
