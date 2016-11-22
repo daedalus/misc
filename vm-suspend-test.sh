d3ae3af97bba549ec266d055f1f2776372d1ac80
Dario Clavijo <daedalus2027@gmail.com>
2016-11-22 17:38:26 -0300

    ESXI VM SUSPEND TEST

---
+++ vm-suspend-test.sh
@@ -0,0 +1,33 @@
+#! /bin/ash
+# VM SUSPEND TEST
+# 
+
+rm -f /tmp/listid
+touch /tmp/listid
+
+
+######## Listing the running vms################
+esxcli vm process list |grep -v "^\s.*"| grep -v "^$" > /tmp/list
+
+########## cleaning the id.s file by keeping only the id
+for name in `cat list`;do
+        vim-cmd vmsvc/getallvms | grep $name | grep vmx | grep -v "^$" | awk '{print $1 }' >> /tmp/listid
+done
+
+for id in `cat /tmp/listid`;do
+        ###### suspending vms##########
+        echo "Suspending the running machines: "
+        vim-cmd vmsvc/power.suspend $id
+done
+
+for id in `cat /tmp/listid`;do
+        ###### suspending vms##########
+        echo "Powering on machines: "
+        vim-cmd vmsvc/power.on $id
+done
+
+
+###### removing files############
+#
+rm /tmp/list
+rm /tmp/listid
