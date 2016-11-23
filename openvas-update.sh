6e8e7cca703cc9c215df123d11901f2c37a246e8
Darío Clavijo <clavijodario@gmail.com>
2016-11-23 15:12:59 -0300

    Update openvas-update.sh

--- openvas-update.sh
+++ openvas-update.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 # Author Dario Clavijo 2016
-
 set -x
-
 LOG=/var/log/openvas/sync.log
+DATE=$(date '+%d/%m/%Y %H:%M:%S')
+
+echo "==========================================================================" >> $LOG
+echo "|                 OpenVAS update Runing on $DATE          |" >> $LOG
+echo "==========================================================================" >> $LOG
 
 /usr/sbin/openvas-certdata-sync  >> $LOG
 /usr/sbin/openvas-nvt-sync  >> $LOG
 /usr/sbin/openvas-scapdata-sync >> $LOG
+
