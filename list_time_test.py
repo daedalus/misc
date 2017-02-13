4857ff780716957e4e31d0c403d739ab709073e2
Darío Clavijo <clavijodario@gmail.com>
2017-02-13 14:24:42 -0300

    Update list_time_test.py

--- list_time_test.py
+++ list_time_test.py
@@ -1,54 +1,38 @@
-#!/usr/bin/env python 
-# list insertion time test 
-# Dario Clavijo 2017
- 
-import random 
-import time 
- 
-list = [] 
- 
-def test(e): 
-        global list 
-        s1 = time.time() 
-        if e not in list: 
-                list.append(e) 
-        l = len(list)  
-        if l >= 100000: 
-                list = [] 
- 
-        s2 = time.time() 
-        d = s2 - s1 
-        return d 
- 
- 
-max = 0 
-c = 0 
-while True: 
-        c = c +1 
-        e = bytearray(random.getrandbits(8) for _ in xrange(20)) 
-        d = test(e) 
-        if d > max: 
-                max = d 
-        print "count %d, time iter: %2.6f, max_time: %2.6f" %  (c,d,max)
-~                                                                                                                                                                                                                   
-~                                                                                                                                                                                                                   
-~                                                                                                                                                                                                                   
-~                                                                                                                                                                                                                   
-~                                                                                                                                                                                                                   
-~                                                                                                                                                                                                                   
-~                                                                                                                                                                                                                   
-~                                                                                                                                                                                                                   
-~                                                                                                                                                                                                                   
-~                                                                                                                                                                                                                   
-~                                                                                                                                                                                                                   
-~                                                                                                                                                                                                                   
-~                                                                                                                                                                                                                   
-~                                                                                                                                                                                                                   
-~                                                                                                                                                                                                                   
-~                                                                                                                                                                                                                   
-~                                                                                                                                                                                                                   
-~                                                                                                                                                                                                                   
-~                                                                                                                                                                                                                   
-~                                                                                                                                                                                                                   
-~                                                                                                                                                                                                                   
--- (insert) VISUAL --                                                                                                                                                                   31        31,66-73      All
+#!/usr/bin/env python
+# list insertion time test
+
+import random
+import time
+
+list = []
+
+def test(e):
+        global list
+        s1 = time.time()
+        if e not in list:
+                list.append(e)
+        l = len(list)
+        if l >= 100000:
+                list = []
+
+        s2 = time.time()
+        d = s2 - s1
+        return d
+
+
+max = 0
+c = 0
+acc = 0
+avg = 0
+while True:
+        c = c +1
+        e = bytearray(random.getrandbits(8) for _ in xrange(20))
+        d = test(e)
+        acc += d
+
+        if d > max:
+                max = d
+        if (c % 1000 == 0):
+            avg = acc/1000
+
+        print "count %d, iter_time: %2.6f, max_time: %2.6f, avg_time: 2.6f" %  (c,d,max,avg)                                                                                                                                                                         1,1           All
