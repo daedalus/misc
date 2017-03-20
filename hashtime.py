c6fc022d9a989697f22b530a25430ba39de42f17
Darío Clavijo <clavijodario@gmail.com>
2017-03-20 15:55:32 -0300

    Update hashtime.py

--- hashtime.py
+++ hashtime.py
@@ -3,11 +3,11 @@ import timeit
 import hashlib
 import time
 
-
 print "Algo\tSpeed"
 
 for h in hashlib.algorithms_available:
         size = 100
+        
         def test(h):
                 data=open('/dev/urandom','r').read(1024*1024*size)
                 algo = hashlib.new(h);
@@ -21,7 +21,6 @@ for h in hashlib.algorithms_available:
                 dt = t2-t1
 
                 return dt
-
 
         dt = measure(h)
         print "%s\t%s MB/s" % (h,round(size/dt,2))
