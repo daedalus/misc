7384f61ce89e223f63267a8d66ead8e85efa03fd
Darío Clavijo <clavijodario@gmail.com>
2017-05-25 15:13:40 -0300

    some improvements

--- uploadmailbackup.sh
+++ uploadmailbackup.sh
@@ -1,21 +1,40 @@
 #!/bin/bash
-# author Darío Clavijo 2016
+# Author Dario Clavijo 2017
 # GPLv3
 
 set -x
 
+# setting up variables later needed
 USER=dclavijo
 RSA_PUB=1C8E0F39
 
-MAILDIR=/home/$USER/mail/
+MAILDIR=/home/$USER/.mail/
 TMP_DISK=vdb1
-DST_DISK=my_nfs_server
+DST_DISK=nfs_op
 IMG=/media/$TMP_DISK/$USER/backup/mail.img
-DST=/media/$DST_DISK/$USER/backup/mail_backup.gpg
+DST=/media/$DST_DISK/Respaldos/$USER/backup/mail_backup.gpg
 
-su $USER -c "getmail -n"
-sync
-umount $IMG
-lrzip -f -L 9 $IMG
-gpg -z 0 --always-trust --yes  --output $DST --encrypt --recipient $RSA_PUB $IMG.lrz
+# mounting the image
 mount $IMG $MAILDIR
+
+# mail getting stage
+su $USER -c "getmail -n -g $MAILDIR/personal/.getmail/" 
+su $USER -c "getmail -n -g $MAILDIR/work/.getmail/" 
+
+sync
+
+# deduping stage
+duperemove -dhr -b 4k --hashfile=tmp/.mail_backup_hashfile_4k  $MAILDIR
+
+# umounting image
+umount $IMG
+#lrzip -f -L 9 $IMG
+#bzip2 $IMG
+
+# compressing and encrypting stage
+time lzop -k $IMG -c | lrzip -f -L 9 - > $IMG.lzo.lrz
+time gpg -z 0 --always-trust --yes  --output $DST --encrypt --recipient $RSA_PUB $IMG.lzo.lrz
+
+# cleaning up
+rm $IMG.lzo.lrz
+#rm /tmp/.hashfile4k
