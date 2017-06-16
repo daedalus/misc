deb1740587894b17adc2122e90bae782f58733ba
Dario Clavijo <daedalus2027@gmail.com>
2017-06-16 14:57:55 -0300

    update mkcryptdisk.sh

--- mkcryptdisk.sh
+++ mkcryptdisk.sh
@@ -6,14 +6,18 @@ set -x
 DISK=$1
 KEYFILE=$2
 
+cryptsetup luksClose $DISK
 cryptsetup luksFormat -y -v /dev/$DISK
 cryptsetup luksAddKey /dev/$DISK $KEYFILE
 cryptsetup --key-file $KEYFILE luksOpen /dev/$DISK $DISK
 mkfs.btrfs /dev/mapper/$DISK
 mkdir /media/$DISK
-mount /dev/mapper/$DISK /media/$DISK 
+#mount /dev/mapper/$DISK /media/$DISK 
 
-BLKID=$(blkid /dev/$DISK -value | head -n 1)
+BLKID=$(blkid /dev/$DISK -o value | head -n 1)
+echo "$DISK UUID=$BLKID $KEYFILE  luks" >> /etc/crypttab
+BLKID=$(blkid /dev/mapper/$DISK -o value | head -n 1)
+echo "UUID=$BLKID /media/$DISK btrfs defaults,compress=lzo 0 0" >> /etc/fstab
 
-echo "$DISK UUID=$BLKID $KEYFILE  luks" > /etc/crypttab
-echo "/dev/mapper/$DISK /media$DISK btrfs defaults 0 0" > /etc/fstab
+#mount -a
+
