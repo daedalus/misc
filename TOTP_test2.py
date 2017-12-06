eef77252898708de8f75065d3db841700291e9e5
Dario Clavijo <daedalus2027@gmail.com>
2017-12-06 16:47:37 -0300

    TOTP to QRCODE

---
+++ TOTP_test2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# Author Dario Clavijo 2017
+# GPLv3
+
+# This programs uses the google authenticator sqlite3 database
+import qrcode
+import pyotp
+import sqlite3
+import sys
+import os
+import tempfile
+
+#mp = tempfile.mkstemp()[1]
+
+#os.system('cat %s | gpg --decrypt > %s' % (sys.argv[1],tmp))
+
+conn = sqlite3.connect(sys.argv[1])
+c = conn.cursor()
+
+for row in c.execute("select * from accounts"):
+
+	account = row[1]
+	secret = row[2]
+	issuer = row[6]
+
+	data = "otpauth://totp/%s?secret=%s&issuer=%s" % (account,secret,issuer)
+	img = qrcode.make(data)
+	img.show()
