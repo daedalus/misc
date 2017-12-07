fc6c35e4655e91f1eedaaa29a55d0160701811d6
Dario Clavijo <daedalus2027@gmail.com>
2017-12-07 18:01:41 -0300

    brocade version get

---
+++ brocade_version.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+#
+# script que obtiene la version de fabric os de los fcsw brocade
+#
+# v1.0 initial version
+
+import fileinput
+import sys
+import re
+import paramiko
+
+# execute a remote command by ssh
+def exec_ssh(host,user,passwd,command):
+	ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(hostname=host, username=user, password=passwd)
+        stdin, stdout, stderr = ssh.exec_command(command)
+	return stdout.readlines()
+
+for line in fileinput.input():
+	line = line.rstrip()
+	try:
+		ret = line,exec_ssh(line,'USER','PASS','version')
+		for version in ret[1]:
+			if re.search('Fabric', version):
+        			print ret[0],version.rstrip()
+	except:
+		print line,'failed'
