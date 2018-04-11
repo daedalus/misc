f45a2beb90974f49b9386ca2b1b3f4a31d5b538c
Darío Clavijo <clavijodario@gmail.com>
2018-04-11 18:18:46 -0300

    Update and rename hash160.bin to hash160.sh

--- hash160.sh
+++ hash160.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
+# Author Dario Clavijo 2018
 set -x
 
 BIN=$HOME/tmp/hash160.bin
 TXT=$HOME/tmp/hash160.txt
 BLF=$HOME/tmp/bloom.blf
 
+cp --reflink $BIN $BIN.bkp
+cp --reflink $BLF $BLF.bkp
+
 sort -u $1 | xxd -r -p >> $BIN
 xxd -c 20 -ps $BIN > $TXT
 sort -u $TXT | xxd -r -p > $BIN
 
-$HOME/code/BTC/brainflayer/hex2blf $TXT $BLF
+code/BTC/brainflayer/hex2blf $TXT $BLF
 
 rm $TXT
