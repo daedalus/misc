295ac60decd29f867e211945671c926dc99bf69f
Darío Clavijo <daedalus2027@gmail.com>
2018-06-23 19:42:54 -0300

    Update imm_pasadas.py

--- imm_pasadas.py
+++ imm_pasadas.py
@@ -7,13 +7,23 @@ import urllib
 import urllib2
 import sys
 
+l = len(sys.argv)
+#print l
+
 codigo_parada=sys.argv[1]
 tipo_dia=sys.argv[2]
 hora=sys.argv[3]
+if l > 4:
+    linea = sys.argv[4]
+
 url_base='http://www.montevideo.gub.uy/transporteRest/'
 
-print url_base+'pasadas/'+str(codigo_parada)+'/'+tipo_dia
-r=urllib2.urlopen(url_base+'pasadas/'+str(codigo_parada)+'/'+tipo_dia+'/'+hora)
-j=json.loads(r.read())
+if l > 4:
+    url = url_base+'pasadas/'+str(codigo_parada)+'/'+tipo_dia+'/'+str(linea)+'/'+hora
+else:
+    url = url_base+'pasadas/'+str(codigo_parada)+'/'+tipo_dia+'/'+hora
+
+j=json.loads(urllib2.urlopen(url).read())
 for pasada in j:
     print "Linea:", pasada['linea'], "Hora:",str(pasada['horaDesc']), "Destino:",pasada['destino']
+
