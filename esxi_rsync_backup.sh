9a791fe459d47711e7980f81e64166ead86cd211
Dario Clavijo <daedalus2027@gmail.com>
2018-07-10 17:20:01 -0300

    Update esxi_rsync_backup.sh

--- esxi_rsync_backup.sh
+++ esxi_rsync_backup.sh
@@ -20,30 +20,29 @@ VMS=$(vim-cmd vmsvc/getallvms | awk '{ if(NR>1) print $1 }')
 for VMID in $VMS
 do
     echo "VMID: $VMID"
-
-    VMPATHNAME=$(vim-cmd vmsvc/get.summary $VMID | grep -e vmPathName)
     POWERSTATE=$(vim-cmd vmsvc/get.summary $VMID | grep -e powerState | awk '{ print $3 }' | sed -e 's/[",]//g')
     echo "powerstate: $POWERSTATE"
 
     if ["$POWERSTATE" == "poweredOn"]
     then
-	DATASTORE=$(echo $VMPATHNAME | awk '{ print $3 }' | sed 's/[^a-zA-Z0-9]//g')
-	VMPATHDIR=$(echo $VMPATHNAME | awk '{ print $4 }' | sed -e 's/\// /g' | awk '{ print $1 }')
+        VMPATHNAME=$(vim-cmd vmsvc/get.summary $VMID | grep -e vmPathName)
+	    DATASTORE=$(echo $VMPATHNAME | awk '{ print $3 }' | sed 's/[^a-zA-Z0-9]//g')
+	    VMPATHDIR=$(echo $VMPATHNAME | awk '{ print $4 }' | sed -e 's/\// /g' | awk '{ print $1 }')
     
-	VMPATH="/vmfs/volumes/$DATASTORE/$VMPATHDIR/"
-	echo "path: $VMPATH"
+	    VMPATH="/vmfs/volumes/$DATASTORE/$VMPATHDIR/"
+	    echo "path: $VMPATH"
 
-	# hacemos el snapshot de la VM con quiesce sin imagen de la RAM    
-	vim-cmd vmsvc/snapshot.create $VMID backup backup 0 1
-	SNAPSHOT_ID=$(vim-cmd vmsvc/snapshot.get $VMID | grep -e "Id" | awk '{  print $4 }')
-	echo $SNAPSHOT_ID
+	    # hacemos el snapshot de la VM con quiesce sin imagen de la RAM    
+	    vim-cmd vmsvc/snapshot.create $VMID backup backup 0 1
+	    SNAPSHOT_ID=$(vim-cmd vmsvc/snapshot.get $VMID | grep -e "Id" | awk '{  print $4 }')
+	    echo $SNAPSHOT_ID
         
-	# se hace el backup backup
-	/bin/rsync $RSYNC_OPTS $VMPATH $DST
+	    # se hace el backup backup
+	    /bin/rsync $RSYNC_OPTS $VMPATH $DST
 
-	# se borra el snapshot
-	#vim-cmd vmsvc/snapshot.remove $VMID $SNAPSHOT_ID
-	vim-cmd vmsvc/snapshot.removeall $VMID 
+	    # se borra el snapshot
+	    #vim-cmd vmsvc/snapshot.remove $VMID $SNAPSHOT_ID
+	    vim-cmd vmsvc/snapshot.removeall $VMID 
     else
     	/bin/rsync $RSYNC_OPTS $VMPATH $DST
     fi		
