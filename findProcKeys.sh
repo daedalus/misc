896b4ae03725d85a507c93f234a9028d018ae0e1
Darío Clavijo <daedalus2027@gmail.com>
2018-08-29 21:18:05 -0300

    Create findProcKeys.sh

---
+++ findProcKeys.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# Author Dario Clavijo 2018
+
+set -x
+
+function _ReadProcMemByPid {
+    python readprocmem.py $1 /tmp/
+    for f in /tmp/*.dump; do
+    
+        RSAKEY=$f.rsa.key
+        AESKEY=$f.aes.key
+
+        rsakeyfind $f > $RSAKEY
+        aeskeyfind -q $f > $AESKEY
+
+        if [ ! -s $RSAKEY ] ; then rm $RSAKEY; fi
+        if [ ! -s $AESKEY ] ; then rm $AESKEY; fi
+    done
+    rm /tmp/*.dump
+}
+
+function _ProcsByName {
+    for pid in $(ps aux | grep -e $1 | awk '{ print $2 }'); do
+        _ReadProcMemByPid $pid
+    done
+}
+
+function _AllProcs {
+    for pid in $(ps aux | tail -n +2 | awk '{ print $2 }'); do
+        _ReadProcMemByPid $pid
+    done
+}
+
+if [ ! -f /usr/bin/rsakeyfind ]; then
+        echo "rsakeyfind not found!"
+        exit 0
+fi
+if [ ! -f /usr/bin/aeskeyfind ]; then
+        echo "aeskeyfind not found!"
+        exit 0
+fi
+
+if [ "$1" = "-p" ]; then
+    _ReadProcMemByPid $2
+elif [ "$1" = "-n" ]; then
+    _ProcsByName $2
+else 
+    _AllProcs  
+fi
