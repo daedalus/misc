06a682663cbc180e44dff4d329249bba6adbdafb
Dario Clavijo <daedalus2027@gmail.com>
2018-09-03 17:27:53 -0300

    Update brainflayer_parallelized.py

--- brainflayer_parallelize.py
+++ brainflayer_parallelize.py
@@ -4,33 +4,18 @@ import os
 from subprocess import Popen
 import sys
 
-filename = "I2"
-
 #config
 binary="code/BTC/brainflayer/brainflayer"
 bloomfile="tmp/bloom.blf"
 tabfile="tmp/tabfile.tab"
 binfile="tmp/hash160.bin"
-outputfile="tmp/brains/%s" % filename
+outputfile=sys.argv[2]
+h = sys.argv[1]
 
 cmd = "%s -b %s -m %s -f %s -v -a -o %s -I" % (binary,bloomfile,tabfile,binfile,outputfile) + " %s"
 
-#print cmd
-#exit(0)
-
-mode = 2
-
-if mode == 1:
-	for i in range(0,59):
-		h = "".zfill(0+i-1) + "1" + "".zfill(64-i)
-		print h
-		_cmd = (cmd % h)
-		print _cmd
-		Popen(_cmd.split(" "))
-else:
-	h = sys.argv[1]
-	n = 10
-	for i in range(1,n+1):
-		_cmd = (cmd % h) + (" -n %d/%d" % (i,n))
-		print _cmd
-		Popen(_cmd.split(" "))
+n = 10
+for i in range(1,n+1):
+	_cmd = (cmd % h) + (" -n %d/%d" % (i,n))
+	print _cmd
+	Popen(_cmd.split(" "))
