d520f88bc481d387ec176d13650764479cb6a47d
Dario Clavijo <daedalus2027@gmail.com>
2018-10-22 17:40:24 -0300

    A simple Reverse Polish Notation calculator

---
+++ rpn.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# Author Dario Clavijo 2018
+
+stack = []
+while True:
+	ops = str(raw_input(">"))
+	if ops != "":
+		ops = ops.split()
+		for op in ops:
+			stack.append(op)
+	if op == "+":
+		a = int(stack[-2])
+	 	b = int(stack[-3])
+		r = a + b
+		stack.pop()
+		stack.pop()
+		stack.pop()
+		stack.append(r)	
+		print r
+	if op == "*":
+	        a = int(stack[-2])
+                b = int(stack[-3])
+                r = a * b
+		stack.pop()
+		stack.pop()
+		stack.pop()
+                stack.append(r)
+                print r
+	if op == "-":
+	        a = int(stack[-2])
+                b = int(stack[-3])
+		stack.pop()
+		stack.pop()
+		stack.pop()
+                r = a - b
+                stack.append(r)
+                print r
+	if op == "/":
+		a = int(stack[-2])
+                b = int(stack[-3])
+                stack.pop()
+                stack.pop()
+                stack.pop()
+                r = a / b
+                stack.append(r)
+                print r
+	print stack
