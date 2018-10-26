b6280dc623ea0cef404e95019eb6b3f7e1f7425d
Darío Clavijo <daedalus2027@gmail.com>
2018-10-26 01:07:32 -0300

    Update winerun.sh

--- winerun.sh
+++ winerun.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 # Author Dario Clavijo 2018
 set -x
-export WINEDEBUG=fps 
+export mesa_glthread=true
+export WINEDEBUG=-all,+fps
 export DISPLAY=:3
-LOG=/tmp/.$1.fps.log
-GAME=$1 # YOUR GAME's EXE
+LOG=/tmp/GAME.log
+LOG2=/tmp/qemu.log
+
+GAMEDIR='Games/Game'
+GAMEEXE='Game.exe'
+GAMEOPTS='-dx9'
+
+cd $GAMEDIR
 
 function display_stats {
     TEMP=$(acpi -t | awk '{ print $4 }')
@@ -12,14 +19,26 @@ function display_stats {
     echo "$FPS $TEMP C" | osd_cat --lines=1 --font='lucidasanstypewriter-bold-8'
 }
 
-function wine_run {
-    startx /usr/bin/wine64-development $1 -- $DISPLAY 2>&1 | sed -u -n -e '/trace/ s/.*approx //p' >> $LOG &
+function gamerun {
+	if [ $1 == 'normal' ]; then
+		startx /usr/bin/wine64-development $GAMEEXE $GAMEOPTS -- $DISPLAY 2>&1 | sed -u -n -e '/trace/ s/.*approx //p' >> $LOG &
+	fi
+	if [ $1 == 'virt']; then
+		startx /usr/bin/qemu-x86_64 /usr/bin/wine64-development $GAMEEXE $GAMEOPTS -- $DISPLAY 2>&1 | sed -u -n -e '/trace/ s/.*approx //p' >> $LOG &
+	fi
+	if [ $1 == 'virt-debug' ]; then
+		startx /usr/bin/qemu-x86_64 -strace -d pcall,exec,guest_errors,mmu -D $LOG2 /usr/bin/wine64-development $GAMEEXE $GAMEOPTS -- $DISPLAY 2>&1 | sed -u -n -e '/trace/ s/.*approx //p' >> $LOG &
+	fi
 }
 
-wine_run $1
+if [[ $1 == '' ]]; then
+	gamerun 'normal'
+else
+	gamerun $1
+fi
 
 while sleep 5; do
     display_stats
 done
 
-rm LOG
+#rm $LOG 
