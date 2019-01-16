f0059b468c0e3827fabb866d9ed9b1f497d6008c
Darío Clavijo <daedalus2027@gmail.com>
2019-01-16 02:25:27 -0300

    Update QuasiLinearGCD.py

--- testQuasiLinearGCD.py
+++ testQuasiLinearGCD.py
@@ -3,6 +3,7 @@
 # based on https://algorithmsoup.wordpress.com/2019/01/15/breaking-an-unbreakable-code-part-1-the-hack/ 
 # and paper https://pure.tue.nl/ws/files/67735608/741686-1.pdf
 
+import sys
 import time
 import gmpy2
 from gmpy2 import mpz
@@ -12,15 +13,21 @@ def naiveGCD(x,y):
         x, y = y, x % y                     
     return x 
 
-#GCD = naiveGCD
-GCD = gmpy2.gcd
-
 def batchGCDcompute(k,verbose=False):
-    def secmult(x,y):
+
+    # Sequence multiply usually notated by big greek letter PI.
+    def SeqMult(x):
+        tmp = 1
+        for i in xrange(0,len(x)):
+            tmp *= x[i]
+        return tmp
+
+    # Sequence multiply sugested int the blogpost
+    def SeqMult1(x,y):
         tmp = 1
         for i in xrange(0,len(x)):
             if y != i:
-                tmp = tmp * j[i]
+                tmp *= x[i]
         return tmp
 
     r = []
@@ -29,24 +36,38 @@ def batchGCDcompute(k,verbose=False):
 
     n = len(k)
 
-    for i in xrange(0,n,2):
+    # we make pairs
+    for i in xrange(0,n-1,2):
         j.append(k[i] + k[i+1])
+
+    # precomputing the sequence multiply
+    tmp0 = SeqMult(j)
+    if verbose:
+        print tmp0
 
     if verbose:
         print "j",j
 
+    # The function SecMult1 will waste resources doing the same multiplications at left and right of the array
+    # precomputing the sequence muliply and dividing by the term we want to exlude shields the same result.
     for i in xrange(0,len(j)):
-        r.append(GCD(j[i],secmult(j,i)))
+        #a = GCD(j[i],SeqMult1(j,i))
+        b = GCD(j[i],tmp0/j[i]) 
+        #if verbose:
+            #print "a,b",a,b
+        r.append(b)
+
+    #sys.exit(0)
 
     if verbose:
         print "r",r
 
     for i in xrange(0,len(j)):
         if i % 2 != 0:
-            tmp0 = GCD(k[i],r[(i+1)/2] * k[i+1])
+            tmp1 = GCD(k[i],r[(i+1)/2] * k[i+1])
         else:
-            tmp0 = GCD(k[i],r[i/2] * k[i-1])
-        s.append(tmp0)
+            tmp1 = GCD(k[i],r[i/2] * k[i-1])
+        s.append(tmp1)
     return s
 
 a = 113257592704268871468251608331599268987586668983037892662393533567233998824693
@@ -55,9 +76,9 @@ ab = a*b
 print "inputs:"
 print hex(a),hex(b),hex(ab)
 
-#k = [ab*a,ab*a*a,ab*ab,b*b]
-def test(n,verbose=False):
-    k = [a*a,b*b,ab*a,ab*b] * n
+def test0(n,verbose=False):
+    # we chose some arbitrary parameters with comon factors to test.
+    k = [a*a,b*b,ab*a,ab*b] * n 
     if verbose:
         print "pubs:"
     pubs = []
@@ -72,11 +93,25 @@ def test(n,verbose=False):
         for p in s:
             print hex(s)
 
-print "size,timetotal,time/size"
-for i in range(1,100):
-    n = i * 100
+
+def measure(func,n):
     t0 = time.time()
-    test(n)
+    func(n)
     t1 = time.time()
     td = t1-t0
-    print i*100,td,td/n
+    print n,td,td/n
+
+def batchperftest():
+    batchsize = 100
+    batches = 10
+    print "size,timetotal,time/size"
+    for i in range(1,batches+1):
+        n = i * batchsize
+        measure(test0,n)
+
+print "gmpy2 gcd"
+GCD = gmpy2.gcd
+batchperftest()
+print "naive gcd"
+GCD = naiveGCD
+batchperftest()
