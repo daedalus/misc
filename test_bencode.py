c8da7535d0dd312231ffec904001f3d018b75a94
Darío Clavijo <daedalus2027+gh@gmail.com>
2019-06-11 23:30:16 -0300

    bencode torrent test

---
+++ test_bencode.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import bencode
+import sys
+import json
+
+f = open(sys.argv[1],"r") 
+raw_data = f.read()
+data = bencode.bdecode(raw_data)
+#for i in data['info']['pieces'].split(" "):
+#    print i.encode("hex")
+#print data
+print data
