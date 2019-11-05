5bad698ba94493b28166f339fad29436460c296a
Darío Clavijo <clavijodario@gmail.com>
2019-11-05 16:32:23 -0300

    Update certfinder.sh

--- certfinder.sh
+++ certfinder.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+set -x
 DEST=/tmp/CERTS
 mkdir $DEST
-find / -name *.pem | xargs -I{} cp -u {} $DEST
-find / -name *.der | xargs -I{} cp -u {} $DEST
-find / -name *.cert | xargs -I{} cp -u {} $DEST
-find / -name *.crt | xargs -I{} cp -u {} $DEST
-find / -name *.RSA | xargs -I{} cp -u {} $DEST
+find / -name *.pem | xargs -I{} cp --backup  {} $DEST
+find / -name *.der | xargs -I{} cp --backup  {} $DEST
+find / -name *.cert | xargs -I{} cp --backup {} $DEST
+find / -name *.crt | xargs -I{} cp --backup {} $DEST
+find / -name *.RSA | xargs -I{} cp --backup {} $DEST
 tar czf CERTS.tar.gz $DEST
 rm -rf $DEST
