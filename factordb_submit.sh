7081b9a11c330f76bc00d81f8cb42ffb69d56bdc
Darío Clavijo <clavijodario@gmail.com>
2019-11-09 22:29:18 -0300

    Update factordb_submit.sh

--- factordb_submit.sh
+++ factordb_submit.sh
@@ -3,5 +3,5 @@ set -x
 USERID=$2
 URL="http://factordb.com/report.php"
 DATA=$(cat $1)
-curl -X POST -F "report=$DATA" -F "format=11" -b "fbuser=$USERID" $URL | grep -e "Found" | grep -e factors > /tmp/result
+curl -X POST -F "report=$DATA" -F "format=0" -b "fbuser=$USERID" $URL | grep -e "Found" | grep -e factors > /tmp/result
 cat $1 | mail daedalus2027@gmail.com -s "Factoring results"
