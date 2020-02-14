7b8aea7e7304b4e2b0a0ac6644087818dbe30a0e
Dario Clavijo <daedalus2027@gmail.com>
2020-02-13 23:04:53 -0200

    rpn calc v2

--- rpn2.py
+++ rpn2.py
@@ -1,115 +1,118 @@
 #!/usr/bin/env python
 # A simple reverse polish notation calculator
 # Author Dario Clavijo 2018
+import fileinput
 
 r = 0
 a = 0
 b = 0
-
-import fileinput
-
 debug = False
 
-ops = []
-stack = []
-for line in fileinput.input():
-        line = line.rstrip()
-	if line != "":
-		ops += line.split(" ")
+def load_ops(fp):
+	ops = []
+	for line in fp:
+        	line = line.rstrip()
+		if line != "":
+			ops += line.split(" ")
+	return ops
 
-for op in ops:
-	stack.append(op)
-	if op == "+":
-		a = int(stack[-2])
-		b = int(stack[-3])
-		r = a + b
-		stack.pop()
-		stack.pop()
-		stack.pop()
-		stack.append(r)
-                if debug:	
-			print r
-	if op == "*":
-		a = int(stack[-2])
-                b = int(stack[-3])
-		r = a * b
-		stack.pop()
-		stack.pop()
-		stack.pop()
-		stack.append(r)
-		if debug:
-        		print r
-	if op == "-":
-	        a = int(stack[-2])
-	        b = int(stack[-3])
-		stack.pop()
-		stack.pop()
-		stack.pop()
-		r = a - b
-        	stack.append(r)
-		if debug:
-	        	print r
-	if op == "/":
-		a = int(stack[-2])
-               	b = int(stack[-3])
-		stack.pop()
-       		stack.pop()
-               	stack.pop()
-		r = a / b
-     		stack.append(r)
-		if debug:
-               		print r
-        if op == "%": 
-                a = int(stack[-2])
-                b = int(stack[-3])
-                stack.pop()
-                stack.pop()
-                stack.pop()
-                r = a % b
-                stack.append(r)
-		if debug:
-                	print r
-        if op == "**": 
-                a = int(stack[-2])
-                b = int(stack[-3])
-                stack.pop()
-                stack.pop()
-                stack.pop()
-                r = a ** b
-                stack.append(r)
-		if debug:
-                	print r
-        if op == "^": 
-                a = int(stack[-2])
-                b = int(stack[-3])
-                stack.pop()
-                stack.pop()
-                stack.pop()
-                r = a ^ b
-                stack.append(r)
-		if debug:
-                	print r
-        if op == ">>": 
-                a = int(stack[-2])
-                b = int(stack[-3])
-                stack.pop()
-                stack.pop()
-                stack.pop()
-                r = a >> b
-                stack.append(r)
-		if debug:
-                	print r
-        if op == "<<": 
-                a = int(stack[-2])
-                b = int(stack[-3])
-                stack.pop()
-                stack.pop()
-                stack.pop()
-                r = a << b
-                stack.append(r)
-		if debug:
-                	print r
+def proc(ops):
+	stack = []
+	for op in ops:
+		stack.append(op)
+		if op == "+":
+			a = int(stack[-2])
+			b = int(stack[-3])
+			r = a + b
+			stack.pop()
+			stack.pop()
+			stack.pop()
+			stack.append(r)
+        	        if debug:	
+				print r
+		if op == "*":
+			a = int(stack[-2])
+        	        b = int(stack[-3])
+			r = a * b
+			stack.pop()
+			stack.pop()
+			stack.pop()
+			stack.append(r)
+			if debug:
+        			print r
+		if op == "-":
+	       		a = int(stack[-2])
+	        	b = int(stack[-3])
+			stack.pop()
+			stack.pop()
+			stack.pop()
+			r = a - b
+        		stack.append(r)
+			if debug:
+	        		print r
+		if op == "/":
+			a = int(stack[-2])
+               		b = int(stack[-3])
+			stack.pop()
+       			stack.pop()
+               		stack.pop()
+			r = a / b
+     			stack.append(r)
+			if debug:
+               			print r
+        	if op == "%": 
+        	        a = int(stack[-2])
+        	        b = int(stack[-3])
+        	        stack.pop()
+        	        stack.pop()
+        	        stack.pop()
+        	        r = a % b
+        	        stack.append(r)
+			if debug:
+                		print r
+        	if op == "**": 
+                	a = int(stack[-2])
+                	b = int(stack[-3])
+                	stack.pop()
+                	stack.pop()
+                	stack.pop()
+                	r = a ** b
+                	stack.append(r)
+			if debug:
+                		print r
+        	if op == "^": 
+        	        a = int(stack[-2])
+        	        b = int(stack[-3])
+        	        stack.pop()
+        	        stack.pop()
+        	        stack.pop()
+        	        r = a ^ b
+        	        stack.append(r)
+			if debug:
+        	        	print r
+        	if op == ">>": 
+        	        a = int(stack[-2])
+        	        b = int(stack[-3])
+        	        stack.pop()
+        	        stack.pop()
+        	        stack.pop()
+        	        r = a >> b
+                	stack.append(r)
+			if debug:
+                		print r
+        	if op == "<<": 
+        	        a = int(stack[-2])
+        	        b = int(stack[-3])
+        	        stack.pop()
+        	        stack.pop()
+        	        stack.pop()
+               	 	r = a << b
+                	stack.append(r)
+			if debug:
+                		print r
 
+ops = load_ops(fileinput.input())
+stack = proc(ops)
 if len(stack) > 1:
 	print stack
 else:
