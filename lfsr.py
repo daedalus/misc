9a097be6887882959cd2a60f63057bff3615dd87
Darío Clavijo <clavijodario@gmail.com>
2020-05-14 23:54:11 -0300

    fibonacci lfsr 16 bit

---
+++ lfsr.py
@@ -0,0 +1,16 @@
+
+
+def lfsr16bit(start_state=0xACE1):
+  lfsr = start_state
+  bit = 0
+  period = 0
+  run = True
+  while run:
+    bit = (lfsr >> 0) ^ (lfsr >> 2) ^ (lfsr >> 3) ^ (lfsr >> 5) & 0xFFFF
+    lfsr = (lfsr >> 1) | (bit << 15) & 0xFFFF
+    period+=1
+    print(period,hex(lfsr),hex(bit))
+    run = (lfsr != start_state)
+  return period
+ 
+print (lfsr16bit())
