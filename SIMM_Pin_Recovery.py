75db5c0c1f4845d5c3d788d6f0159fb67fbea5bc
Darío Clavijo <clavijodario@gmail.com>
2021-05-04 00:48:09 -0300

    Update SIMM_Pin_Recovery.py: handle CTRL+C

--- SIMM_Pin_Recovery.py
+++ SIMM_Pin_Recovery.py
@@ -7,36 +7,48 @@ import argparse
 import sys
 import time
 import humanfriendly 
+import signal
 
 from smartcard.System import readers
-#from smartcard.util import toHexString
 
-def crack_pin(n,l,waittime=None,reset=False):
-  """ The function just forms a APDU with the pin then sends it to the reader and waits for the status. """  
-  ns = n
-  ti = time.time()
-  r=readers()
-  print("[+] Reader:",r)
-  connection = r[0].createConnection()
-  connection.connect()
+class Attack():
+  def __init__(self, connection, startpin, digits, wait, reset):
+    self.wait = wait
+    self.reset = reset
+    self.startpin = startpin
+    self.ns = startpin
+    self.l = digits
+    self.stoping = False
+    self.Continue = True
+    self.Found = False
+    self.connection = connection
 
-  L = 10**(l)-1
-  c = True
-  s = "X%dd" % l
-  s = s.replace("X","%0")
-  print("[+] startpin: %d, max: %d, wait: %s, reset: %s" % (n,L,str(waittime),str(reset)))
-
-  def runtime():
-    td = time.time() - ti
-    nd = n - ns
+  def runtime(self):
+    td = time.time() - self.ti
+    nd = self.nc - self.ns
     ndtd = nd/td
     htd = humanfriendly.format_timespan(td)
-    print("[+] Runtime: %s, tried pins: %d, rate: %.4f" % (htd,nd,ndtd)) 
+    print("[+] Runtime: %s, tried pins: %d, rate: %.4f    " % (htd,nd,ndtd)) 
 
-  while n <= L and c: # keep looping if the c is set
- 
+  def signal_handler(self, sig, frame):
+    self.stoping = True
+    print('[!] You pressed Ctrl+C!  ')
+
+  def xmit(self,Command):
+    if not self.stoping:
+      try:
+        r = self.connection.transmit(Command) # send the command
+      except:
+        if self.stoping == False:
+          print("[!] Connection error!, last pin checked: %d." % (self.nc-1))
+          self.stoping = True
+        r = None
+    else:
+      r = None
+    return r
+
+  def encode_pin_cmd(self,n,s):
     N = s % n
-
     A = 0x30 + int(N[0])
     B = 0x30 + int(N[1])
     C = 0x30 + int(N[2])
@@ -54,39 +66,61 @@ def crack_pin(n,l,waittime=None,reset=False):
       G = 0x30 + int(N[6])
     if l > 7:
       H = 0x30 + int(N[7])
-
+ 
     COMM = [0xA0, 0x20, 0x00, 0x01, 0x08, A, B, C, D, E, F, G, H] # APDU with command pkt
 
-    try:
-      data, sw1, sw2 = connection.transmit(COMM) # send the command
-    except:
-      print("[!] Connection error!, last pin checked: %d." % n-1)
-      runtime()
-      sys.exit(-1)
-    scommand = str(list(map(hex,COMM)))
- 
-    sys.stderr.write("Pin: %s, Command: %s, Status: %02x %02x\r" % (N,scommand, sw1, sw2))
-    c = (sw1 == 0x98 and sw2 == 0x08) # if invalid pin then c=True
+    return COMM
 
-    if sw2 == 0x40: # status for card blocked
-      print("[!] Card blocked, check PUK!...")
-      runtime()
-      sys.exit(-1)
+  def crack_pin(self):
+    """ The function just forms a APDU with the pin then sends it to the reader and waits for the status. """  
+    self.ti = time.time()
+    n = self.startpin
+    L = 10**(l)-1
+    c = True
+    s = "X%dd" % l
+    s = s.replace("X","%0")
+    print("[+] startpin: %d, max: %d, wait: %s, reset: %s" % (n,L,str(self.wait),str(self.reset)))
 
-    if c == False: # Status for successful attack
-      print("[*] The PIN is: " % N)
-      runtime()
-      sys.exit(0)
-    n += 1
+    while n <= L and self.Continue: # keep looping if the c is set
+      self.nc = n
+      COMM = self.encode_pin_cmd(n,s)
+      r = self.xmit(COMM)
+      if r != None:
+        data,sw1,sw2 = r
+        scommand = str(list(map(hex,COMM)))
 
-    if waittime != None: 
-      time.sleep(waittime)
+        if not self.stoping:
+          sys.stderr.write("Pin: %d, Command: %s, Status: %02x %02x\r" % (n,scommand, sw1, sw2))
+        self.Continue = (sw1 == 0x98 and sw2 == 0x08) # if invalid pin then c=True
+        self.Found = not self.Continue
 
-    if reset: # reset the chip
-      RSET = [0x3B, 0x9F, 0x96, 0x80, 0x1F, 0xC6, 0x80, 0x31, 0xE0, 0x73, 0xFE, 0x21, 0x1B, 0x64, 0x41, 0x04, 0x81, 0x00, 0x82, 0x90, 0x00, 0x04]
-      data, sw1, sw2 = connection.transmit(RSET)
+        if sw2 == 0x40: # status for card blocked
+          print("[!] Card blocked, check PUK!...")
+          #self.runtime()
+          #sys.exit(-1)
+          self.stoping = True
+      else:
+        #c = False
+        self.stoping = True
 
-  runtime() # prints runtime information
+      if self.Found: # Status for successful attack
+        print("[*] The PIN is: " % N)
+        #self.runtime()
+        #sys.exit(0)
+        self.stoping = True
+
+      if self.wait != None: 
+        time.sleep(waittime)
+
+      if self.reset: # reset the chip
+        RSET = [0x3B, 0x9F, 0x96, 0x80, 0x1F, 0xC6, 0x80, 0x31, 0xE0, 0x73, 0xFE, 0x21, 0x1B, 0x64, 0x41, 0x04, 0x81, 0x00, 0x82, 0x90, 0x00, 0x04]
+        data, sw1, sw2 = self.xmit(RSET)
+
+      n += 1
+
+      if self.stoping == True:
+        self.runtime() # prints runtime information
+        self.Continue = False
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description="SIMM Pin recovery tool")
@@ -96,6 +130,11 @@ if __name__ == "__main__":
   parser.add_argument("--reset", help="Reset the chip after try")
 
   args = parser.parse_args()
+
+  r = readers()
+  print("[+] Reader:",r)
+  connection = r[0].createConnection()
+  connection.connect()
 
   if args.startpin:
     #n= 00274710
@@ -112,5 +151,7 @@ if __name__ == "__main__":
   else:
     l = 4
 
-  crack_pin(n,l,waittime=args.wait,reset=args.reset)
+  attack = Attack(connection, n, l, args.wait, args.reset)
+  signal.signal(signal.SIGINT, attack.signal_handler)
+  attack.crack_pin()
   print("[-] Program end.")
