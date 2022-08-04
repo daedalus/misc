0525fe0e9d5636e01a4cbc59aa81f82a7c2a6487
Darío Clavijo <clavijodario@gmail.com>
2022-08-04 18:39:52 -0300

    Update rot13.py

--- rot13.py
+++ rot13.py
@@ -3,10 +3,12 @@
 # simple ROT13 algo or Caesar Cipher
 
 
-def rot13(message):
+def ROT13(message):
     p = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ "
     r = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM "
     return "".join([r[p.find(c)] for c in message])
 
 
-print rot13(rot13("Dario Clavijo"))
+if __name__ == '__main__':
+  message = "Dario Clavijo"
+  assert ROT13(ROT13(message)) == message
