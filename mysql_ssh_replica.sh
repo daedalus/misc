ca48ef611c9f94e648b7f54ccb20c31809488c66
dclavijo <clavijodario@gmail.com>
2022-10-31 15:29:25 -0300

    update mysql ssh replica script

--- mysql_ssh_replica.sh
+++ mysql_ssh_replica.sh
@@ -4,4 +4,5 @@ set -x
 HOST_DEST=$1
 DATABASE=$2
 ssh-copy-id root@$HOST_DEST
+ssh root@$HOST_DEST -C "echo drop $DATABASE; create $DATABASE | mysql -u root -p "
 time mysqldump -u root $DATABASE -p | gzip -c | ssh root@$HOST_DEST -C "gzip -d | mysql -u root -p $DATABASE"
