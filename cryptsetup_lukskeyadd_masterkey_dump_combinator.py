fa2960ff5e0889418e920aee5c307f3eb6fc0dd4
Darío Clavijo <clavijodario@gmail.com>
2022-12-07 17:29:22 -0300

    Create cryptsetup_lukskeyadd_masterkey_dump_combinator.py

---
+++ cryptsetup_lukskeyadd_masterkey_dump_combinator.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# Author Dario Clavijo 2022
+
+import sys
+import os
+from binascii import unhexlify
+
+CMD="/sbin/cryptsetup luksAddKey /dev/sda5 --master-key-file /tmp/master.key" 
+
+def main():
+  
+  def writefile(filename, data):
+    fp = open(filename,"wb")
+    fp.write(data)
+    fp.close()
+  
+  K = [line.rstrip() for line in open(sys.argv[1])]
+  for k1 in K:
+    for k2 in K:
+      if k1 != k2:
+        k = k1 + k2
+        data = unhexlify(k)
+        print("k:",k)
+        print(len(k),len(data))
+        writefile("/tmp/master.key", data)
+        os.system(CMD)
+       
+if __name__ == "__main__":
+  main()
