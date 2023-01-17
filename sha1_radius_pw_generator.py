ff4bdcedf9ff3bdbede9619167b62fcf4b485265
dclavijo <clavijodario@gmail.com>
2023-01-17 15:22:53 -0300

    add sha1 freeradius password generator

---
+++ sha1_radius_pw_generator.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Autor Dario Clavijo 2023
+
+import hashlib, sys, base64
+
+salt = sys.argv[1].encode("utf-8")
+passwd = sys.argv[2].encode("utf-8")
+
+print(base64.b64encode(hashlib.sha1(passwd+salt).digest()+salt).decode("utf8"))
+
