0a34f0fe6e4a767a03ff138055796aa36774524c
dclavijo <clavijodario@gmail.com>
2023-05-05 16:45:13 -0300

    Refactor code to avoid duplicated queries to HIBP. Add tqdm and disable urllib3 warnings.

--- passbolt_api_hibp_test.py
+++ passbolt_api_hibp_test.py
@@ -6,6 +6,9 @@ import passboltapi
 import json
 import pwnedpasswords
 import sys
+from tqdm import tqdm
+import urllib3
+urllib3.disable_warnings()
 
 passbolt = passboltapi.PassboltAPI(config_path="config.ini", new_keys=True)
 
@@ -17,15 +20,40 @@ def dec_resource_secret(id_):
   except:
     return None
 
-for r in passbolt.get(url="/resources.json?api-version=v2")['body']:
+sys.stderr.write("Gathering resources from passbolt...\n")
+D = {}
+for r in tqdm(passbolt.get(url="/resources.json?api-version=v2")['body']):
+  #print(r)
   password = dec_resource_secret(r['id'])
   if password != None:
-    rating = pwnedpasswords.check(password)
-    if rating > 0:
-      msg = "<" + r['id'] + "><" + r['name'] + "><" + password + ">"
-      sys.stderr.write("-*60")
-      sys.stderr.write(msg + "\n")
-      sys.stdout.write(msg + "\n")
-      sys.stderr.flush()
-      sys.stdout.flush()
+    t = (r['id'], r['name'],r['username'],r['uri'],r['description'])
+    if password not in D:
+      D[password] = [t]
+    else:
+      D[password].append(t)
 
+sys.stderr.write("Checking passwords hashes against havibeenpwned...\n")
+broken=[]
+for password in tqdm(D):
+  rating = pwnedpasswords.check(password, plain_text=True)
+  if rating > 0:
+    broken.append(password)
+
+sys.stderr.write("Results:\n")
+
+for password in broken:
+  sys.stderr.write("-" * 60 + "\n")
+  sys.stdout.write("-" * 60 + "\n")
+  sys.stderr.write("broken password: %s\n" % password)
+  sys.stdout.write("broken password: %s\n" % password)
+  for t in D[password]:
+    msg = "id:%s, name:%s, username:%s, uri: %s, description: %s\n" % t
+    #sys.stderr.write("-" * 60 + "\n")
+    #sys.stdout.write("-" * 60 + "\n")
+    sys.stderr.write(msg)
+    sys.stdout.write(msg)
+  sys.stderr.flush()
+  sys.stdout.flush()
+
+sys.stdout.write("Total broken: %d\n" % len(broken))
+sys.stdout.flush()
