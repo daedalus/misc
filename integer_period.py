bf41b0160a0ac574a215e58cf29568f78cc19344
Darío Clavijo <clavijodario@gmail.com>
2023-08-15 15:10:46 -0300

    Update integer_period.py

--- integer_period.py
+++ integer_period.py
@@ -7,7 +7,8 @@ n = 6230699590438730861365449567108381380284939 ** 2
 
 def find_period(n):
     shifted = n
-    l = int(math.log(n, 2))
+    #l = int(math.log(n, 2))
+    l = n.bit_length()
     mask = (1 << l) - 1
 
     for period in range(1, l):
