d2c0b6ad4a8ea1a97a525c8a7f6bd588e287338b
Darío Clavijo <clavijodario@gmail.com>
2025-04-15 17:57:58 -0300

    Add vscode_install.sh

---
+++ vscode_install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -x
+apt install wget
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
+sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+apt update && sudo apt install code
